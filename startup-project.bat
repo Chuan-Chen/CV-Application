2d6c38a04a261463125adcc7a65fccfd1c43f089
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2022-09-22 22:54:26 -0400

    Update startup-project.bat

--- startup-project.bat
+++ startup-project.bat
@@ -1,10 +1,26 @@
 @echo off
+echo (
+
+ _______                                   __             ______     __                           __                         
+|       \                                 |  \           /      \   |  \                         |  \                        
+| $$$$$$$\  ______    ______    _______  _| $$_         |  $$$$$$\ _| $$_     ______    ______  _| $$_    __    __   ______  
+| $$__| $$ /      \  |      \  /       \|   $$ \        | $$___\$$|   $$ \   |      \  /      \|   $$ \  |  \  |  \ /      \ 
+| $$    $$|  $$$$$$\  \$$$$$$\|  $$$$$$$ \$$$$$$         \$$    \  \$$$$$$    \$$$$$$\|  $$$$$$\\$$$$$$  | $$  | $$|  $$$$$$\
+| $$$$$$$\| $$    $$ /      $$| $$        | $$ __        _\$$$$$$\  | $$ __  /      $$| $$   \$$ | $$ __ | $$  | $$| $$  | $$
+| $$  | $$| $$$$$$$$|  $$$$$$$| $$_____   | $$|  \      |  \__| $$  | $$|  \|  $$$$$$$| $$       | $$|  \| $$__/ $$| $$__/ $$
+| $$  | $$ \$$     \ \$$    $$ \$$     \   \$$  $$       \$$    $$   \$$  $$ \$$    $$| $$        \$$  $$ \$$    $$| $$    $$
+ \$$   \$$  \$$$$$$$  \$$$$$$$  \$$$$$$$    \$$$$         \$$$$$$     \$$$$   \$$$$$$$ \$$         \$$$$   \$$$$$$ | $$$$$$$ 
+                                                                                                                   | $$      
+                                                                                                                   | $$      
+                                                                                                                    \$$      
+)
+
 echo Creating React project
 echo Adding babelrc
-echo >.babelrc
-echo {"preset": [@babel/preset-react],"plugins": [@babel/plugin-proposal-class-properties]} > .babelrc
+echo {"preset": [@babel/preset-react],"plugins": [@babel/plugin-proposal-class-properties]}>.babelrc
 echo Adding webpack.config.js
 echo >webpack.config.js
+echo node_modules >.gitignore
 
 echo Installing node modules
 call npm i react react-dom 
